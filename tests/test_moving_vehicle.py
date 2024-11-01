c2f0f7f547040da132d7293a6376a48598fc7155
brunozc <brunozuada@gmail.com>
2024-11-01 15:19:13 +0100

    merged the diff

--- tests/test_moving_vehicle.py
+++ tests/test_moving_vehicle.py
@@ -57,32 +57,35 @@ class TestSpringDamperModel():
 
         # set vehicle location parameters
         loc_vehicle = 0.0
-
-        velocity = json_input_file["parameters"]["velocity"]
+        velocity = 100 / 3.6
 
         # Euler beam parameters
-        n_beams =  beam_parameters["n_elements"]
-        length_beam = beam_parameters["total_length_beam"]
-        E = beam_parameters["E"]
-        I = beam_parameters["I"]
+        n_beams = 10
+        length_beam = 50
+        E = 2.87e9
+        I = 2.9
         L = length_beam / n_beams
-        rho = beam_parameters["rho"]
-        A = beam_parameters["A"]
-        omega_1 = beam_parameters["omega_1"]
-        omega_2 = beam_parameters["omega_2"]
+        rho = 2303
+        A = 1
+        omega_1 = 0
+        omega_2 = 0
 
         # Create the euler beam structure
-        euler_beam_structure = UtilsFct.create_simply_supported_euler_beams(n_beams, E, I, L, rho, A, omega_1, omega_2)
+        euler_beam_structure = UtilsFct.create_simply_supported_euler_beams(n_beams, E, I, L, rho, A, omega_1,omega_2)
 
         u_structure = np.zeros(euler_beam_structure.K_global.shape[0])
+        # u_structure[0::2] = 1
         v_structure = np.zeros(euler_beam_structure.K_global.shape[0])
         a_structure = np.zeros(euler_beam_structure.K_global.shape[0])
 
         # initialize time integration solver for simply supported beam
         solver = NewmarkExplicit()
 
-        # get dt
+        # set time integration parameters
+        n_steps = 2000
+        time = np.linspace(0, 1.8, n_steps)
         dt = time[1] - time[0]
+
         json_input_file["dt"] = dt
 
         # initialize arrays to store results
@@ -90,7 +93,7 @@ class TestSpringDamperModel():
         all_u_bogie = []
 
         # loop over time steps
-        for t in range(len(time) - 1):
+        for t in range(n_steps-1):
 
             # get vertical displacement at vehicle location on beam
             u_vert = UtilsFct.get_result_at_x_on_simply_supported_euler_beams(u_structure, euler_beam_structure,
@@ -124,69 +127,33 @@ class TestSpringDamperModel():
             all_u_beam.append(u_structure.tolist())
             all_u_bogie.append(json_input_file["state"]["u"][-3])
 
-        return all_u_beam, all_u_bogie
-
-    def test_spring_damper_model(self):
-        """
-        Tests a moving vehicle on a simply supported beam. Where the vehicle consists of a wheel which
-        is in contact with the beam and a mass which is connected to the wheel with a spring and damper
-
-        Based on Biggs "Introduction do Structural Dynamics", pp pg 322
-
-        :return:
-        """
-
-        json_input_file = {"dt": 0.001,
-                           "loads": {"1": [0, 0, 0]},
-                           "parameters": {"n_carts": 1,
-                                          "cart_inertia": 0,
-                                          "cart_mass": 0,
-                                          "cart_stiffness": 0,
-                                          "cart_damping": 0,
-                                          "bogie_distances": [0],
-                                          "bogie_inertia": 0,
-                                          "bogie_mass": 3000,
-                                          "wheel_distances": [0],
-                                          "wheel_mass": 5750,
-                                          "wheel_stiffness": 1595e5,
-                                          "wheel_damping": 1000,
-                                          "contact_coefficient": 9.1e-8,
-                                          "contact_power": 1,
-                                          "gravity_axis": 1,  # 0 = x, 1 = y, 2 = z
-                                          "initialisation_steps": 1,
-                                          "velocity": 100 / 3.6,
-                                          },
-                           "state": {"a": [],
-                                     "u": [],
-                                     "v": []
-                                     },
-                           "t": 0,
-                           "theta": {"1": [0.0, 0.0, 0.0]},
-                           "time_index": 0,
-                           "u": {"1": [0.0, 0, 0.0]}
-                           }
-
-        beam_parameters = {"n_elements": 10,
-                           "total_length_beam": 50,
-                           "E": 2.87e9,
-                           "I": 2.9,
-                           "rho": 2303,
-                           "A": 1,
-                           "omega_1": 0,
-                           "omega_2": 0
-                           }
-
-        time = np.linspace(0, 1.8, 2000)
-
-        # run the spring damper model
-        all_u_beam, all_u_bogie = self.run_spring_damper_model(json_input_file, beam_parameters, time)
-
-        # inspect results if needed
-        if INSPECT_RESULTS:
-            self.inspect_results(json_input_file, beam_parameters, time, all_u_beam, all_u_bogie)
-
         # load expected results
-        expected_results = json.load(open('tests/test_data/expected_data_test_spring_damper.json', "r"))
+        expected_results = json.load(open('tests/test_data/expected_data_test_spring_damper.json',"r"))
+
+        if INSPECT_RESULTS:
+            all_u_beam = np.array(all_u_beam)
+            all_u_bogie = np.array(all_u_bogie)
+
+            fig, ax = plt.subplots(2, 1, figsize=(6, 9))
+            # plot displacement results of the centre of the beam and the bogie
+            ax[0].plot(time[:-1], -all_u_beam[:,len(u_structure)//2+1], color='r', label="numerical")
+            ax[1].plot(time[:-1], -all_u_bogie, color='r', label="numerical")
+
+            ss = TwoDofVehicle()
+            ss.vehicle(json_input_file["parameters"]["bogie_mass"], json_input_file["parameters"]["wheel_mass"], velocity,
+                       json_input_file["parameters"]["wheel_stiffness"], json_input_file["parameters"]["wheel_damping"])
+            ss.beam(E, I, rho, A, length_beam)
+            ss.compute()
+
+            ax[0].plot(ss.time, ss.displacement[:, 0], color='b', linestyle="--", label="analytical")
+            ax[1].plot(ss.time, ss.displacement[:, 1], color='b', linestyle="--", label="analytical")
+            ax[0].set_ylabel("Displacement beam [m]")
+            ax[1].set_ylabel("Displacement bogie [m]")
+            ax[1].set_xlabel("Time [s]")
+            ax[0].legend()
+            ax[1].legend()
+            plt.tight_layout()
+            plt.show()
 
         # Assert results
         np.testing.assert_almost_equal(all_u_beam, expected_results["u_beam"])
