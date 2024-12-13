2e2cec60b44761d2e59e288649d7fbf914d33c74
brunozc <brunozuada@gmail.com>
2024-12-13 21:51:09 +0100

    Refactor  tests

--- tests/test_copy_package.py
+++ tests/test_copy_package.py
@@ -1,71 +1,54 @@
 import os
-import sys
 import site
 import subprocess
-from shutil import rmtree
 
 
 class TestCopy():
 
-    def test_install_package_ten_dof(self):
+    def test_install_package(self):
         """
         Test install of 10 DOF uvec
         """
 
         # uninstall the package
-        subprocess.run(['pip', 'uninstall', '-y', "UVEC"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        subprocess.run(['pip', 'uninstall', '-y', "UVEC"])
+
 
         # Install the package in normal mode
-        subprocess.run(['pip', 'install', '.'])
+        subprocess.run(['pip', 'install', '.', '--force-reinstall'])
 
+        # 10 DOF
         from UVEC import uvec_ten_dof_vehicle_2D as uvec
         assert uvec.UVEC_NAME == "uvec_ten_dof_vehicle_2D"
         path_uvec = uvec.get_path_file(uvec.UVEC_NAME)
         assert os.path.join(site.getsitepackages()[0], r"UVEC/uvec_ten_dof_vehicle_2D") == path_uvec
 
-    def test_install_package_ten_dof_editable(self):
-        """
-        Test install of 10 DOF uvec in editable mode
-        """
-
-        # uninstall the package
-        subprocess.run(['pip', 'uninstall', '-y', "UVEC"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        # Install the package in normal mode
-        subprocess.run(['pip', 'install', '-e', '.'])
-
-        from UVEC import uvec_ten_dof_vehicle_2D as uvec
-        assert uvec.UVEC_NAME == "uvec_ten_dof_vehicle_2D"
-        path_uvec = uvec.get_path_file(uvec.UVEC_NAME)
-        assert os.path.join(os.getcwd(), r"UVEC/uvec_ten_dof_vehicle_2D") == path_uvec
-
-    def test_install_package_two_dof(self):
-        """
-        Test install of 2 DOF uvec
-        """
-
-        # uninstall the package
-        subprocess.run(['pip', 'uninstall', '-y', "UVEC"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        # Install the package in normal mode
-        subprocess.run(['pip', 'install', '.'])
-
+        # 2 DOF
         from UVEC import uvec_two_dof_vehicle_2D as uvec
         assert uvec.UVEC_NAME == "uvec_two_dof_vehicle_2D"
         path_uvec = uvec.get_path_file(uvec.UVEC_NAME)
         assert os.path.join(site.getsitepackages()[0], r"UVEC/uvec_two_dof_vehicle_2D") == path_uvec
 
-    def test_install_package_two_dof_editable(self):
+
+    def test_install_package_editable(self):
         """
-        Test install of 2 DOF uvec in editable mode
+        Test install of 10 DOF uvec in editable mode
         """
 
         # uninstall the package
-        subprocess.run(['pip', 'uninstall', '-y', "UVEC"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        subprocess.run(['pip', 'uninstall', '-y', "UVEC"])
+
 
         # Install the package in normal mode
-        subprocess.run(['pip', 'install', '-e', '.'])
+        subprocess.run(['pip', 'install', '-e', '.', '--force-reinstall'])
 
+        # 10 DOF
+        from UVEC import uvec_ten_dof_vehicle_2D as uvec
+        assert uvec.UVEC_NAME == "uvec_ten_dof_vehicle_2D"
+        path_uvec = uvec.get_path_file(uvec.UVEC_NAME)
+        assert os.path.join(os.getcwd(), r"UVEC/uvec_ten_dof_vehicle_2D") == path_uvec
+
+        # 2 DOF
         from UVEC import uvec_two_dof_vehicle_2D as uvec
         assert uvec.UVEC_NAME == "uvec_two_dof_vehicle_2D"
         path_uvec = uvec.get_path_file(uvec.UVEC_NAME)
